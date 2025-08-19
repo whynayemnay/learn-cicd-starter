c8ce1db917e26c1f528862445e9414f9323a0a7a
Kirils <kirils.prihodjko@gmail.com>
2025-08-19 13:19:30 +0200

    check linting in workflow

--- main.go
+++ main.go
@@ -21,6 +21,12 @@ type apiConfig struct {
 	DB *database.Queries
 }
 
+// unused function to test linter
+func unused() {
+	// this function does nothing
+	// and is called nowhere
+}
+
 //go:embed static/*
 var staticFiles embed.FS
 
